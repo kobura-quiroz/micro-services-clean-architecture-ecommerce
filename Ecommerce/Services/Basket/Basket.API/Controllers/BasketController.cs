d2478622c1d4423d98f71051a3c5236871ca1336
ivan-quiroz-unosquare <ivan.quiroz@unosquare.com>
2026-03-11 20:47:15 -0600

    Add Basket.API project with Redis, Swagger, and CQRS
    - Introduced Basket.API targeting .NET 9.0 with Redis caching, and OpenAPI/Swagger documentation.
    - Added BasketController with CQRS endpoints using MediatR.
    - Configured dependency injection, appsettings, and launch profiles.

---
+++ Ecommerce/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.DTOs;
+using Basket.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BasketController(IMediator meditor)
+    {
+        _mediator = meditor;
+    }
+
+    [HttpGet("{userName}")]
+    public async Task<ActionResult<ShoppingCartDto>> GetBasket(string userName)
+    {
+        var query = new GetBasketByUserNameQuery(userName);
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCartDto>> CreateOrUpdateBasket([FromBody] CreateShoppigCartCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete("{userName}")]
+    public async Task<IActionResult> DeleteBasket(string userName)
+    {
+        var command = new DeleteBasketByUserNameCommand(userName);
+        await _mediator.Send(command);
+        return Ok();
+    }
+}
